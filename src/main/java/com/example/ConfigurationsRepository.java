de0185153d29cf28ccd77c0b846663d278c58ae6
delaemon <takeshi.music.5@gmail.com>
2015-10-23 23:42:28 +0900

    fix

--- src/main/java/com/example/ConfigurationsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.Repository;
-
-public interface ConfigurationsRepository extends Repository<Configurations, String> {
-    Page<Configurations> findAll(Pageable pageable);
-}
